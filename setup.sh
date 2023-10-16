37cb98c797f8995103e65c1d4e27d2ebf7b2a405
Huy Dinh Tran <91640334+huydinhtran@users.noreply.github.com>
2023-10-15 19:26:22 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/buildroot/buildroot.git
 cp -r buildroot/ gem5-tutorial/buildroot/
 cd gem5-tutorial
+mkdir resources ckpts rundir guest-scripts
 cd resources
 wget http://dist.gem5.org/dist/v22-0/arm/aarch-system-20220707.tar.bz2
 tar -xf aarch-system-20220707.tar.bz2
